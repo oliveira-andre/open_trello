fa7a8aaa359212b25561aa0feac6a6a7f6b6d4ba
Andre Oliveira <desmonplays@gmail.com>
2020-04-16 20:11:16 -0400

    (new feature) when turbolinks load find de element with id app and inject render on it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,25 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import Vue from 'vue/dist/vue.esm';
+import App from '../app.vue';
+import TurbolinksAdapter from 'vue-turbolinks';
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
+Vue.use(TurbolinksAdapter);
+Vue.use(VueResource);
+Vue.use(VueRouter);
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import router from '../routes';
+
+document.addEventListener('turbolinks:load', () => {
+  Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+  });
+});
