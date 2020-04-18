faa577294b5a208575dc2a6f26ef1f9e1998df91
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 16:11:41 -0400

    (new feature) getting projects and loading on screen

--- app/javascript/components/home/index.vue
+++ app/javascript/components/home/index.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="home">
     <div class="container">
+      {{ this.projects }}
     </div>
   </div>
 </template>
 
  <script>
+  import toastr from 'toastr/toastr';
+
   export default {
     data() {
       return {
+        projects: [],
+        token: localStorage.getItem('token'),
       }
+    },
+    created: function() {
+      this.$http.get('/api/v1/projects', {
+        headers: { token: this.token }
+      }).then(response => {
+        if(response.status == 200) {
+          console.log(response.body)
+          this.projects = response.body
+        }
+      }, response => {
+        toastr.error('Something got wrong');
+      });
     }
   }     
 </script>
