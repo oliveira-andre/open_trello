8b637a07c841fff2858526c365421110fc39e9a7
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 16:09:55 -0400

    (new feature) getting projects based on user authentication

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ProjectsController < ApplicationController
+      before_action :load_user
+      before_action :load_projects, only: :index
+
+      def index
+        success_response(data: @projects, model: 'Project')
+      end
+
+      def show; end
+
+      private
+
+      def load_projects
+        @projects = @user.projects.active
+      end
+    end
+  end
+end
