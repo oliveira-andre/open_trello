95f2a4176e2a0a9554e42a6ca3e9f5ca1a9580fd
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 16:10:13 -0400

    (new feature) concern to config the response of serializers

---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Response
+  def success_response(data:, model:, includes: '', status: :ok)
+    if data.respond_to?('each')
+      render json: data,
+             each_serializer: "Api::V1::#{model}Serializer".constantize,
+             includes: includes.split(','), status: status
+    else
+      render json: data, serializer: "Api::V1::#{model}Serializer".constantize,
+             includes: includes.split(','), status: status
+    end
+  end
+
+  def error_response
+    render json: {}, status: :unprocessable_entity
+  end
+end
