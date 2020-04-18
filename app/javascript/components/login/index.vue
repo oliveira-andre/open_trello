303b88234f9b65279db3fa7be9aef6516c7c3038
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 16:13:09 -0400

    (new feature) componen login

---
+++ app/javascript/components/login/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="login">
+    <div class="container">
+      <section class="section">
+        <div class="columns is-multiline">
+          <div class="column is-3-desktop is-4-tablet is-offset-5 is-offset-4-tablet">
+            <p class="title has-text-centered">
+              Open Trello
+            </p>
+            <div class="card">
+              <form v-on:submit.prevent.stop="onSubmit">
+                <div class="column is-12">
+                  <p class="subtitle has-text-weight-bold has-text-centered">
+                    Fazer login no Open Trello
+                  </p>
+                </div>
+                <div class="column is-12">
+                  <div class="field">
+                    <div class="control">
+                      <input class="input" type="email" id="email" placeholder="Insert your email" v-model="email">
+                    </div>
+                  </div>
+                </div>
+                <div class="column is-12">
+                  <div class="field">
+                    <div class="control">
+                      <input class="input" type="password" id="password" placeholder="Insert your password" v-model="password">
+                    </div>
+                  </div>
+                </div>
+                <div class="column is-12">
+                  <button class="button is-success is-fullwidth">
+                    Entrar
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+  import toastr from 'toastr/toastr';
+
+  export default {
+    data: function() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      onSubmit: function () {
+        this.$http.post('/api/v1/sessions', {
+          users: {
+            email: this.email,
+            password: this.password
+          }
+        }).then(response => {
+          if(response.status == 201) {
+            toastr.success('You are signed in');
+            localStorage.setItem('token', response.body.authentication_token);
+            this.$router.push('/'); 
+          }
+        }, response => {
+          toastr.error('Invalid email or password');
+        });
+      }
+    }
+  }
+</script>
