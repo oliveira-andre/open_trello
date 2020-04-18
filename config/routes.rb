30a76ff2a152a98fc4ebc249acd4294eff1953c6
Andre Oliveira <desmonplays@gmail.com>
2020-04-17 21:12:33 -0400

    (new feature) resourcing with restfull namespaces the list with only index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      resources :lists, only: %i[index show]
+    end
+  end
+
   root to: 'home#index'
 end
