8f64cf29f3d86c37bedd882afbd819dd9e92eb3d
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 16:10:35 -0400

    (new feature) creating a generic method load user and import the response concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Response
+
+  private
+
+  def load_user
+    @user = User&.find_by(authentication_token: request.headers['token'])
+  end
 end
