84322563e63a9270c4fa6a9a89947c14b9cd6666
Andre Oliveira <desmonplays@gmail.com>
2020-04-17 21:12:07 -0400

    (new feature) configuring simple_authentication_token and has_many projects and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :projects
+  has_many :comments
 end
