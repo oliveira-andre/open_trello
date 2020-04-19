0449c2c514e6e7d391aeba8e0897f1937b501875
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 20:42:50 -0400

    (new feature) checking if is logged in

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    <router-view></router-view>
+    <Header v-if="loggedIn"/>
+      <div>
+        <router-view></router-view>
+      </div>
   </div>
 </template>
 
 <script>
     import Vue from 'vue';
     import VueRouter from 'vue-router';
+    import Header from './components/header/index';
 
     Vue.use(VueRouter);
     export default {
       data: function () {
         return {
+         loggedIn: localStorage.getItem('token') != null
         }
+      },
+      created: function(){ 
+         this.loggedIn = (localStorage.getItem('token') != null);
+      },
+      updated: function() {
+         this.loggedIn = (localStorage.getItem('token') != null);
+      },
+      components: {
+        'Header': Header,
       }
     }
 </script>
