89c38fca2d8ee48f62524e2b3e81bbd3f6ca82f7
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 20:43:06 -0400

    (new feature) showing projects

--- app/javascript/components/home/index.vue
+++ app/javascript/components/home/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div id="home">
     <div class="container">
-      {{ this.projects }}
+      <section class="section">
+        <div class="columns is-multiline">
+          <div class="column is-2" v-for="(project, index) in projects" :key="index">
+            <div class="box">
+              <div class="media-content">
+                <router-link class="title is-size-5" :to="`/projects/${project.title}`">
+                  {{ project.title }}
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
  <script>
   import toastr from 'toastr/toastr';
+  import VueRouter from 'vue-router';
 
   export default {
     data() {
@@ -21,12 +34,11 @@
         headers: { token: this.token }
       }).then(response => {
         if(response.status == 200) {
-          console.log(response.body)
           this.projects = response.body
         }
       }, response => {
         toastr.error('Something got wrong');
       });
-    }
+    },
   }     
 </script>
