3e079a2a693edaf582ae517272c252785ee83518
Andre Oliveira <desmonplays@gmail.com>
2020-04-18 20:44:44 -0400

    (new feature) rendering header with logout button

---
+++ app/javascript/components/header/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item title is-size-5 has-text-centered" href="/">
+        Open Trello
+      </a>
+      <button class="button is-danger is-small" v-on:click.stop.prevent="logout()">Logout</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+      logout: function() {
+        localStorage.clear();
+        Turbolinks.visit('/login');
+      }
+    }
+  }
+</script>
