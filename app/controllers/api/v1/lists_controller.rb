da0413861dd68167a7ac3c680b90bc7a47f33d5a
Andre Oliveira <desmonplays@gmail.com>
2020-04-19 01:11:08 -0400

    (new feature) loading user, loading the project of user, and lists of project

---
+++ app/controllers/api/v1/lists_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class ListsController < ApplicationController
+      before_action :load_user, only: :index
+      before_action :load_project, :load_lists, only: :index
+
+      def index
+        success_response(data: @lists, model: 'List')
+      end
+
+      private
+
+      def load_project
+        @project = @user.projects.find_by(title: params.dig(:project_title))
+      end
+
+      def load_lists
+        @lists = @project.lists.active
+      end
+    end
+  end
+end
+
