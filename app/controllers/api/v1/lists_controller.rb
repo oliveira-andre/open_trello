a5585a9b59167796795ffae50fb87d9a14189cae
Andre Oliveira <desmonplays@gmail.com>
2020-04-21 17:44:31 -0400

    (new feature) returning list with tasks

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class ListsController < ApplicationController
@@ -20,4 +22,3 @@ module Api
     end
   end
 end
-
--- app/serializers/api/v1/list_serializer.rb
+++ app/serializers/api/v1/list_serializer.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class ListSerializer < ActiveModel::Serializer
-      attributes :title
+      attributes :id, :title
+
+      has_many :tasks, if: -> { object.tasks.present? }
     end
   end
 end
