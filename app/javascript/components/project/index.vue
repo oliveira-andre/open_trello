9e74348c2434253bbd2cfdcd5581dbfc0558b92a
Andre Oliveira <desmonplays@gmail.com>
2020-04-22 22:43:08 -0400

    (new feature) adding new lists and tasks

--- app/javascript/components/project/index.vue
+++ app/javascript/components/project/index.vue
@@ -3,14 +3,13 @@
     <div class="columns">
       <draggable v-model="lists" ghost-class="ghost" draggable=".column" class="draggable-column">
       <div class="column is-3" v-for="list in lists">
-        <div class="card has-background-grey-lighter">
+        <div class="card has-background-grey-darker">
           <div class="card-header">
             <p class="card-header-title">{{ list.title }}</p>
           </div>
           <div class="card-content">
             <draggable v-model="list.tasks" ghost-class="ghost" draggable=".draggable-card">
-            <div class="card draggable-card" v-for="task in list.tasks" v-on:click="showDescription">
-              <div class="is-hidden" ></div>
+            <div class="card draggable-card has-background-grey-dark" v-for="task in list.tasks" v-on:click="showDescription">
               <div class="card-header">
                 <p class="card-header-title" :data-id="task.id" :data-description="task.description" :data-title="task.title" :data-list-id="list.id" :data-list-title="list.title">
                 {{ task.title }}
@@ -18,15 +17,26 @@
               </div>
             </div>
             </draggable>
+            <button class="button is-primary is-outlined is-fullwidth" v-on:click="addTask" :data-list-id="list.id">
+              New task
+            </button>
           </div>
         </div>
       </div>
       </draggable>
+      <div class="column is-3"> 
+        <button class="button is-primary is-fullwidth" v-on:click="addList">
+          New list
+        </button>
+      </div>
     </div>
   <div class="modal" id="description-modal">
     <div class="modal-background" v-on:click="closeModal"></div>
     <div class="modal-card">
-      <section class="modal-card-body">
+      <section class="modal-card-header has-background-grey-darker">
+        <input type="text" class="input" v-model="currentTitle" v-on:keyup="saveTitle">
+      </section>
+      <section class="modal-card-body has-background-grey-darker">
         <div class="columns">
           <div class="column is-8">
             <editor-menu-bar :editor="descriptionEditor" v-slot="{ commands, isActive }">
@@ -44,7 +54,9 @@
           <div class="column is-4">
             <div class="select">
               <select v-model="currentListId" v-on:change="updateList">
-                <option :value="list.id" v-for="list in lists" :checked="currentListId == list.id"> {{ list.title }}</option>
+                <option :value="list.id" v-for="list in lists" :checked="currentListId == list.id">
+                  {{ list.title }}
+                </option>
               </select>
             </div>
           </div>
@@ -101,15 +113,17 @@ import toastr from 'toastr/toastr';
         onUpdate: function(event) {
           setTimeout(function(){
           vm.$http.put(
-            `/api/v1/projects/${vm.currentTitle}/lists/${vm.currentListId}/tasks/${vm.currentTaskId}`, {
-              headers: { token: this.token },
+            `/api/v1/projects/${vm.currentTitle}/lists/${vm.currentListId}/tasks/${vm.currentTaskId}`, {}, {
+              headers: { token: vm.token },
               tasks: {
                 description: event.getHTML(),
               }
             }).then(response => {
-              toastr.success('Saved!');
+              if(response.status == 202) {
+                toastr.success('Saved!');
+              }
             }, response => { toastr.error('May accour a problem') });
-          }, 2000)
+          }, 1000)
         },
       });
     },
@@ -128,16 +142,53 @@ import toastr from 'toastr/toastr';
       },
       updateList: function() { 
         this.$http.put(
-          `/api/v1/projects/${this.currentTitle}/lists/${this.currentListId}/tasks/${this.currentTaskId}`, {
+          `/api/v1/projects/${this.currentTitle}/lists/${this.currentListId}/tasks/${this.currentTaskId}`, {}, {
             headers: { token: this.token },
             tasks: {
               list_id: this.currentListId,
             }
           }).then(response => {
-            if(response.status == 200) {
+            if(response.status == 202) {
               toastr.success('Moved with success');
             }
           });
+      },
+      saveTitle: function() { 
+        const vm = this;
+        setTimeout(function(){
+          vm.$http.put(
+            `/api/v1/projects/${vm.currentTitle}/lists/${vm.currentListId}/tasks/${vm.currentTaskId}`, {}, {
+              headers: { token: this.token },
+              tasks: {
+                title: vm.currentTitle
+              }
+            }).then(response => {
+              if(response.status == 202) {
+                toastr.success('Saved!');
+              }
+            }, response => { toastr.error('May accour a problem') });
+          }, 1000)
+      },
+      addTask: function() {
+        let listId = event.target.dataset.listId;
+        this.$http.post(
+          `/api/v1/projects/${this.$route.params.title}/lists/${listId}/tasks`, {}, {
+            headers: { token: this.token },
+          }).then(response => {
+            if(response.status == 201) {
+              Turbolinks.visit(`/projects/${this.$route.params.title}`);
+            }
+        });
+      },
+      addList: function() { 
+        this.$http.post(
+          `/api/v1/projects/${this.$route.params.title}/lists`, {}, {
+            headers: { token: this.token },
+        }).then(response => {
+            if(response.status == 201) {
+              Turbolinks.visit(`/projects/${this.$route.params.title}`);
+            }
+        });
       },
     },
   }
