b2a9957b917b5f5f9db6345b8ab6999a0fabb725
Andre Oliveira <desmonplays@gmail.com>
2020-04-22 22:41:47 -0400

    (new feature) creating a new task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,12 +3,21 @@
 module Api
   module V1
     class TasksController < ApplicationController
-      before_action :load_user
-      before_action :load_list, :load_task
+      before_action :load_user, :load_list
+      before_action :load_task, only: :update
+
+      def create
+        @task = Task.new(list: @list)
+        if @task.save
+          success_response(data: @task, model: 'Task', status: :created)
+        else
+          error_response
+        end
+      end
 
       def update
         if @task.update(task_params)
-          success_response(data: @task, model: 'Task')
+          success_response(data: @task, model: 'Task', status: :accepted)
         else
           error_response
         end
