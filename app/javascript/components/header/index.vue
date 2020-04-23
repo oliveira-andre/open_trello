d02bf38b1ae048a80e7bf91cc1d5f63e6f28f941
Andre Oliveira <desmonplays@gmail.com>
2020-04-22 22:41:58 -0400

    (new feature) setting navbar black

--- app/javascript/components/header/index.vue
+++ app/javascript/components/header/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <nav class="navbar black-background" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item title is-size-5 has-text-centered" href="/">
         Open Trello
