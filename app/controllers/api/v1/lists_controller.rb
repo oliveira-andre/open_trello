d095a8c0c69016127048f03ced5e5bc9172fb7b0
Andre Oliveira <desmonplays@gmail.com>
2020-04-26 19:09:00 -0400

    (new feature) permiting params lists: { title }, loading list and updating current list

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -3,9 +3,9 @@
 module Api
   module V1
     class ListsController < ApplicationController
-      before_action :load_user
-      before_action :load_project
+      before_action :load_user, :load_project
       before_action :load_lists, only: :index
+      before_action :load_list, only: :update
 
       def index
         success_response(data: @lists, model: 'List')
@@ -20,7 +20,19 @@ module Api
         end
       end
 
+      def update
+        if @list.update(list_params)
+          success_response(data: @list, model: 'List', status: :accepted)
+        else
+          error_response
+        end
+      end
+
       private
+
+      def list_params
+        params.require(:lists).permit(:title)
+      end
 
       def load_project
         @project = @user.projects.find_by(title: params.dig(:project_title))
@@ -28,6 +40,10 @@ module Api
 
       def load_lists
         @lists = @project.lists.active
+      end
+
+      def load_list
+        @list = @project.lists.active.find(params.dig(:id))
       end
     end
   end
